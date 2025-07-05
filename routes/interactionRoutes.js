7eb1aa244efba40a2d37609ad145e470b90ad145
hemalata chaudhari <hemhitstemmon01@gmail.com>
2025-07-05 18:08:40 +0530

    interactions

--- routes/interactionRoutes.js
+++ routes/interactionRoutes.js
@@ -2,10 +2,22 @@ const express = require('express');
 const router = express.Router();
 const interactionController = require('../controller/interactionController');
 
-router.post('/', interactionController.createInteraction);
-router.get('/', interactionController.getAllInteractions);
-router.get('/:id', interactionController.getInteractionById);
-router.put('/:id', interactionController.updateInteraction);
-router.delete('/:id', interactionController.deleteInteraction);
+// Create a new interaction
+router.post('/interactions', interactionController.createInteraction);
+
+// Get all interactions with optional filters
+router.get('/interactions', interactionController.getAllInteractions);
+
+// Get a specific interaction by ID
+router.get('/interactions/:id', interactionController.getInteractionById);
+
+// Update an interaction (full update)
+router.put('/interactions/:id', interactionController.updateInteraction);
+
+// Update an interaction (partial update)
+router.patch('/interactions/:id', interactionController.patchInteraction);
+
+// Delete an interaction
+router.delete('/interactions/:id', interactionController.deleteInteraction);
 
 module.exports = router;
